202e881383e02bec73c95c493946cf3a4514abeb
Maciej Gajewski <maciej.gajewski0@gmail.com>
2021-05-06 20:41:55 +0200

    properly creating and using global const

--- llvm_codegen/generate_expression.cc
+++ llvm_codegen/generate_expression.cc
@@ -10,23 +10,25 @@
 namespace Cougar::LlvmCodeGenerator {
 
 llvm::Value *CodeGenerator::generateExpression(Ast::ExStringLiteral &strLit) {
-  // TODO
 
-  // llvm::ArrayType *arrayType = llvm::ArrayType::get(
-  //     llvm::Type::getInt8PtrTy(*mContext), strLit.content.size() + 1);
+  llvm::ArrayType *arrayType = llvm::ArrayType::get(
+      llvm::Type::getInt8PtrTy(*mContext), strLit.content.size() + 1);
 
-  // llvm::GlobalVariable *strConst = new llvm::GlobalVariable(
-  //     *mModule,                                        // module
-  //     arrayType,                                       // type
-  //     true,                                            // is const
-  //     llvm::GlobalValue::LinkageTypes::PrivateLinkage, // linkage
-  //     nullptr                                          // initializer
-  // );
+  llvm::GlobalVariable *strConst = new llvm::GlobalVariable(
+      *mModule,                                        // module
+      arrayType,                                       // type
+      true,                                            // is const
+      llvm::GlobalValue::LinkageTypes::PrivateLinkage, // linkage
+      nullptr                                          // initializer
+  );
 
   llvm::Constant *c =
       llvm::ConstantDataArray::getString(*mContext, strLit.content);
+  strConst->setInitializer(c);
+  strConst->setUnnamedAddr(llvm::GlobalValue::UnnamedAddr::Global);
 
-  return c;
+  // generate "GEP" (getelementpopinter) instruction
+  return mBuilder->CreateConstGEP2_64(arrayType, strConst, 0, 0);
 }
 
 } // namespace Cougar::LlvmCodeGenerator
